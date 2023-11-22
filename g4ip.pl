72897bb6c07cb687efb4c11f8a1c5e4a8655d427
bhaaksema <17254355+bhaaksema@users.noreply.github.com>
2023-11-22 14:33:45 +0000

    G4ip with negation rewrites

--- g4ip.pl
+++ g4ip.pl
@@ -1,39 +1,56 @@
-% -----------------------------------------------------------------
 % g4ip.pl - A sequent calculus prover implemented in Prolog
-% -----------------------------------------------------------------
 
-% operator definitions
-:- op(1000, xfy, &).    % conjunction
-:- op(1100, xfy, '|').  % disjunction
-:- op(1110, xfy, =>).   % implication
+%% OPERATORS
+:- op( 250, fy, ~).     % negation
+:- op( 550, xfy, &).    % conjunction
+:- op(1025, xfy, '|').  % disjunction
+:- op(1050, xfy, =>).   % implication
 
-% -----------------------------------------------------------------
-prove0(F) :- prove([] > [F]).
-% -----------------------------------------------------------------
+% Rewrite negation to implication
+prove(G > (~E)) :- prove(G > (E=>0)), !.
+prove(G > E) :- select1(~A, G, G1), !, prove([A=>0|G1] > E).
 
-% axioms
-prove(G > A) :- member(A, G), !.
+% Axiom 1
 prove(G > _) :- member(0, G).
 
-% conjunction
-prove(G > E) :- select1(A&B, G, G1), !, % lower sequent
-                prove([A, B | G1] > E). % upper sequent
+% Axiom 2
+prove(G > P) :- member(P, G), !.
 
-prove(G > A&B) :- !, prove(G > A), prove(G > B).
+% left 0 implication (invertible)
+prove(G > E) :- select1(P=>B, G, G1), select1(P, G1, G2), !,
+    prove([P, B | G2] > E).
 
-% disjunction
+% left & implication (invertible)
+prove(G > E) :- select1((C&D)=>B, G, G1), !,
+    prove([C=>(D=>B) | G1] > E).
+
+% left | implication (invertible)
+prove(G > E) :- select1((C'|'D)=>B, G, G1), !,
+    prove([C=>B, D=>B | G1] > E).
+
+% left => implication (non-invertible)
+prove(G > E) :- select1((C=>D)=>B, G, G1),
+    prove([D=>B | G1] > C=>D), prove([B | G1] > E).
+
+% right implication (invertible)
+prove(G > (A=>B)) :- !, prove([A | G] > B).
+
+% left conjunction (invertible)
+prove(G > E) :- select1(A&B, G, G1), !,
+    prove([A, B | G1] > E).
+
+% right conjunction (invertible)
+prove(G > (A&B)) :- !, prove(G > A), prove(G > B).
+
+% left disjunction (invertible)
 prove(G > E) :- select1(A'|'B, G, G1), !,
-                prove([A | G1] > E), prove([B | G1] > E).
+    prove([A | G1] > E), prove([B | G1] > E).
+              
+% right disjuction (non-invertible)
+prove(G > (A|B)) :- prove(G > A); prove(G > B).
 
-prove(G > A'|'B) :- !, prove(G > A); prove(G > B).
-
-% implication (G3ip)
-prove(G > E) :- select1(A=>B, G, G1), !,
-                prove(G > A), prove([B | G1] > E).
-
-prove(G > A=>B) :- !, prove([A | G] > B).
-
-% -----------------------------------------------------------------
-% if X in L, then L1 = L \ {X}
+%% AUXILIARY PREDICATES
 select1(X, L, L1) :- append(L2, [X | L3], L), append(L2, L3, L1).
-% -----------------------------------------------------------------
+
+%% PROVER
+prove0(F) :- once(prove([] > F)).
