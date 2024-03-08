5e73b6db85b8d3e452849afbdda22273b1bdeea5
Bas Haaksema <17254355+bhaaksema@users.noreply.github.com>
2024-03-08 15:24:48 +0100

    Left implication cut

--- g4ip.pl
+++ g4ip.pl
@@ -6,25 +6,25 @@
 :- op(1110, xfy, =>).   % implication
 
 % Invertible sequent calculus rules
-prove(G > P) :- member(P, G), !.                             % initial sequent
-prove(G > _) :- member(0, G).                                % (⊥ =>)
-prove(G > (A&B)) :- !, prove(G > A), prove(G > B).           % (=> &)
-prove(G > E) :- select1(A&B, G, G1), !,                      % (& =>)
-    prove([A, B | G1] > E).
-prove(G > E) :- select1(A'|'B, G, G1), !,                    % (| =>)
-    prove([A | G1] > E), prove([B | G1] > E).
-prove(G > (A=>B)) :- !, prove([A | G] > B).                  % (=> ->)
-prove(G > E) :- select1(P=>B, G, G1), select1(P, G1, G2), !, % (P-> =>)
-    prove([P, B | G2] > E).
-prove(G > E) :- select1((C&D)=>B, G, G1), !,                 % (&-> =>)
-    prove([C=>(D=>B) | G1] > E).
-prove(G > E) :- select1((C'|'D)=>B, G, G1), !,               % (|-> =>)
-    prove([C=>B, D=>B | G1] > E).
+prove(G > P) :- member(P, G), !.                          % initial sequent
+prove(G > _) :- member(0, G).                             % (⊥ =>)
+prove(G > (A&B)) :- !, prove(G > A), prove(G > B).        % (=> &)
+prove(G > E) :- select1(A&B, G, G1),                      % (& =>)
+    !, prove([A, B | G1] > E).
+prove(G > E) :- select1(A'|'B, G, G1),                    % (| =>)
+    !, prove([A | G1] > E), prove([B | G1] > E).
+prove(G > (A=>B)) :- !, prove([A | G] > B).               % (=> ->)
+prove(G > E) :- select1(P=>B, G, G1), select1(P, G1, G2), % (P-> =>)
+    !, prove([P, B | G2] > E).
+prove(G > E) :- select1((C&D)=>B, G, G1),                 % (&-> =>)
+    !, prove([C=>(D=>B) | G1] > E).
+prove(G > E) :- select1((C'|'D)=>B, G, G1),               % (|-> =>)
+    !, prove([C=>B, D=>B | G1] > E).
 
 % Non-invertible sequent calculus rules
-prove(G > (A|B)) :- prove(G > A); prove(G > B).              % (=> |)
-prove(G > E) :- select1((C=>D)=>B, G, G1),                   % (->-> =>)
-    prove([D=>B | G1] > C=>D), prove([B | G1] > E).
+prove(G > (A|B)) :- prove(G > A); prove(G > B).           % (=> |)
+prove(G > E) :- select1((C=>D)=>B, G, G1),                % (->-> =>)
+    prove([D=>B | G1] > C=>D), !, prove([B | G1] > E).
 
 % Selection predicate (L = L1 + {X})
 select1(X, L, L1) :- append(L2, [X | L3], L), append(L2, L3, L1).
